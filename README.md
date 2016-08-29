03e767a162ff1f0908688679353ff93cedd6a6bf
Kari Bancroft <kariabancroft@gmail.com>
2016-08-28 19:00:40 -0700

    Create starter files

---
+++ README.md
@@ -0,0 +1,2 @@
+# Game
+This repository contains the starter files for a TDD exercise that simulates creating a game that uses cards.
---
+++ specs/spec_helper.rb
@@ -0,0 +1,7 @@
+require 'minitest'
+require 'minitest/spec'
+require "minitest/autorun"
+require "minitest/reporters"
+require 'minitest/pride'
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
