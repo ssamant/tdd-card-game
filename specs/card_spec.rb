aafce99f3e030fdddad18e7040bf6f4060cb907f
Kari Bancroft <kariabancroft@gmail.com>
2016-09-07 13:00:52 -0700

    Add an example for using a before all

--- specs/card_spec.rb
+++ specs/card_spec.rb
@@ -3,7 +3,10 @@ require_relative '../card'
 
 describe Card do
   describe "#initialize" do
-    let(:card) { Card.new(:hearts, 2) }
+    before(:all) do
+      @card = Card.new(:hearts, 2)
+    end
+    # let(:card) { Card.new(:hearts, 2) }
 
     it "should throw an error when a invalid suit is supplied" do
       proc { Card.new("suit", 2) }.must_raise(ArgumentError)
@@ -17,27 +20,27 @@ describe Card do
     end
 
     it "should be in the valid list of suits" do
-      Card::SUITS.must_include(card.suit)
+      Card::SUITS.must_include(@card.suit)
     end
 
     it "can create a new instance of Card" do
-      card.must_be_instance_of(Card)
+      @card.must_be_instance_of(Card)
     end
 
     it "should have a suit property" do
-      card.must_respond_to(:suit)
+      @card.must_respond_to(:suit)
     end
 
     it "should have a value property" do
-      card.must_respond_to(:value)
+      @card.must_respond_to(:value)
     end
 
     it "suit should have the same value that we set" do
-      card.suit.must_equal(:hearts)
+      @card.suit.must_equal(:hearts)
     end
 
     it "value should have the same value that we set" do
-      card.value.must_equal(2)
+      @card.value.must_equal(2)
     end
   end
 
